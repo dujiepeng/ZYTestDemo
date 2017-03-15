be4fd14d210fea273ad8a6f9d1f31141a709ba51
dujiepeng <347302029@qq.com>
2017-03-15 21:08:52 +0800

    update share files

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Category/ZYDCategory/ShareFiles/ShareFileActivity.h
@@ -0,0 +1,13 @@
+//
+//  ShareFileActivity.h
+//  ChatDemo-UI3.0
+//
+//  Created by 杜洁鹏 on 15/03/2017.
+//  Copyright © 2017 杜洁鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareFileActivity : UIActivity
+
+@end
